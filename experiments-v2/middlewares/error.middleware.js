8e001f8f230883dcb105b61f6ce80a1586dc2893
Fajar Saputra <fajarsaputra2305@gmail.com>
2026-05-05 20:51:46 +0700

    refactor(experiments-v2): enhance global error handler and fix argument type mismatch

--- experiments-v2/middlewares/error.middleware.js
+++ experiments-v2/middlewares/error.middleware.js
@@ -1,16 +1,20 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    const cleanStack = err.stack.split("\n").map((m) => m.trim());
 
-    console.log("==============================================");
-    console.log("Nama    error : ", err.name);
-    console.log("message error : ", err.message);
-    console.log("status  error : ", err.statusCode);
-    console.log("==============================================");
+    // Debugging: Lihat objek asli di terminal
+    console.log("=== ERROR LOG ===");
+    console.error("Name:", err.name);
+    console.error("Message:", message);
+    console.error("Status Code:", statusCode);
+    console.error("Stack", cleanStack);
+
+    console.log("=================");
 
     res.status(statusCode).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-        ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
+        success: "error",
+        message,
     });
 };
 
