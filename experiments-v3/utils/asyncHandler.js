7f858e11daf01a8eb75a53379c183f2e11ec7192
Fajar Saputra <fajarsaputra2305@gmail.com>
2026-05-08 09:13:24 +0700

    chore: cleanup main branch and move experiments to separate branches

--- experiments-v3/utils/asyncHandler.js
+++
@@ -1,5 +0,0 @@
-const asyncHandlerv1 = (fn) => (req, res, next) => fn(req, res, next).catch(next);
-
-const asyncHandlerv2 = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = { asyncHandlerv2 };
