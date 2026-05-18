47151b75c999faeaf01e7261eef81097b95677e5
Fajar Saputra <fajarsaputra2305@gmail.com>
2026-05-18 20:19:43 +0700

    feat: add login and register features

---
+++ services/auth.service.js
@@ -0,0 +1,37 @@
+const { appError } = require("../utils/appError");
+const userRepository = require("../repositories/user.repository");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async ({ username, email, role, password }) => {
+    const user = await userRepository.findByEmail(email);
+    if (user) {
+        throw new appError("User sudah register!", 400);
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    await userRepository.create({ username, email, role, password: hashpassword });
+
+    return { username, email };
+};
+
+const login = async ({ email, password }) => {
+    const user = await userRepository.findByEmail(email);
+    if (!user) throw new appError("User belum register!", 404);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new appError("Password salah", 401);
+
+    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1hr" });
+    return {
+        token,
+        user: {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+        },
+    };
+};
+
+module.exports = { login, register };
