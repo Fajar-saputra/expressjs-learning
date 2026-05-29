141deef6d4e9d2e5d5e214ca0352d4c766b5ca30
Fajar Saputra <fajarsaputra2305@gmail.com>
2026-05-29 22:46:16 +0700

    refactor(auth): clean up source code

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,41 +1,42 @@
-const bcrypt = require('bcrypt');
-const { appError } = require('../utils/appError');
-const jwt= require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const { appError } = require("../utils/appError");
+const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
     let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(" ")[1];    
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
     }
 
     if (!token) {
-        return next(new appError("tidak "))
+        return next(new appError("tidak "));
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
         req.user = decoded;
         console.log(req.user);
-        
-        next()
+
+        next();
     } catch (error) {
-        return next(new appError("Token tidak valid", 401))
+        return next(new appError("Token tidak valid", 401));
     }
-}
+};
 
+const authorize =
+    (...roles) =>
+    (req, res, next) => {
+        if (!req.user) {
+            return next(new appError("Unauthorized", 401));
+        }
 
-const authorize = (...roles) => (req, res, next) => {
-    if (!req.user) {
-        return next(new appError("Unauthorized", 401))
-    }
+        if (!roles.includes(req.user.role)) {
+            return next(new appError("Forbidden", 403));
+        }
 
-    if (!roles.includes(req.user.role)) {
-        return next(new appError("Forbidden", 403))
-    }
+        next();
+    };
 
-    next()
-}
-
-module.exports = {protect, authorize}
+module.exports = { protect, authorize };
