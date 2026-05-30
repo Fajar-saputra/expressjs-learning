6fae0bdaa72feb71670d59df69e590b0255d7616
Fajar Saputra <fajarsaputra2305@gmail.com>
2026-05-30 12:10:29 +0700

    refactor(app): clean up source code

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); 
+app.use("/uploads", express.static("uploads"));
 
-const userRoutes = require('./routes/user.routes');
-const authRoutes = require('./routes/auth.routes');
-const productRoutes = require('./routes/product.routes');
-const { errorHandler } = require('./middleware/error.middleware');
+const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
+const productRoutes = require("./routes/product.routes");
+const { errorHandler } = require("./middleware/error.middleware");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', productRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", productRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 module.exports = app;
